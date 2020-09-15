5bb34b0b7e33a093e8b98bcc30005b1c46ae5f44
Trym Ullestad <trym.ullestad@live.no>
2020-09-15 09:56:48 +0200

    Applied the patch 'swallow'
    This patch makes child processes spawned by terminals take up the space of the parent terminal. This means that windows spawned by a terminal replaces the frozen terminal. When the spawned window is closed, the terminal comes back and is ready to continue.

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
@@ -34,13 +35,15 @@ static const Rule rules[] = {
      *    WM_CLASS(STRING) = instance, class
      *    WM_NAME(STRING) = title
      */
-    /* class                            instance    title           tags mask   isfloating monitor */
-    { "Gimp",                           NULL,       NULL,           0,          1,         -1 },
-    { "Firefox",                        NULL,       NULL,           1,          0,         -1 },
-    { "discord",                        NULL,       NULL,           2,          0,         -1 },
-    { "STM32CubeIDE",                   NULL,       NULL,           1 << 7,     0,         -1 },
-    { "MATLAB R2019a - academic use",   NULL,       NULL,           1 << 8,     0,         -1 },
-};
+    /* class                            instance    title           tags mask   isfloating  isterminal  noswallow   monitor */
+    { "Gimp",                           NULL,       NULL,           0,          1,          0,          0,          -1 },
+    { "Firefox",                        NULL,       NULL,           1,          0,          0,         -1,          -1 },
+    { "discord",                        NULL,       NULL,           2,          0,          0,          0,          -1 },
+    { "STM32CubeIDE",                   NULL,       NULL,           1 << 7,     0,          0,          0,          -1 },
+    { "MATLAB R2019a - academic use",   NULL,       NULL,           1 << 8,     0,          0,          0,          -1 },
+    { "st",                             NULL,       NULL,           0,          0,          1,          0,          -1 },
+    { NULL,                             NULL,       "Event Tester", 0,          0,          0,          1,          -1 }, /* xev */
+    };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
